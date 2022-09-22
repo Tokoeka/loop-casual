3b2680cf819a0740265f0ccd295ae17774665699
Tokoeka <nzgamer666@gmail.com>
2022-09-22 08:26:17 +0100

    update to $path, check NEP

--- src/gash-hop.ts
+++ src/gash-hop.ts
@@ -1,4 +1,16 @@
-import { $class, $classes, $item, ascend, Clan, Lifestyle, Paths, prepareAscension } from "libram";
+import { print, runChoice, visitUrl } from "kolmafia";
+import {
+	$class,
+	$classes,
+	$item,
+  $path,
+	ascend,
+	Clan,
+	get,
+	Lifestyle,
+	prepareAscension,
+	PropertiesManager,
+} from "libram";
 
 /* const timespinnerTargets = [
   "Busta_Rhymes",
@@ -16,42 +28,76 @@ import { $class, $classes, $item, ascend, Clan, Lifestyle, Paths, prepareAscensi
     safariTargets.shift();
 }*/
 
+const PropertyManager = new PropertiesManager();
+
+function setChoice(adv: number, choice: number | string): void {
+	PropertyManager.setChoices({ [adv]: choice });
+}
+
+function NEP() {
+	if (get("_questPartyFair") === "unstarted") {
+		setChoice(1322, "");
+		visitUrl("adventure.php?snarfblat=528");
+		if (get("_questPartyFairQuest") === "food") {
+			runChoice(1);
+			setChoice(1324, 5);
+			// setChoice(1326, 3);
+		} else if (get("_questPartyFairQuest") === "booze") {
+			runChoice(1);
+			setChoice(1324, 5);
+			// setChoice(1327, 3);
+		} else {
+			runChoice(2);
+			setChoice(1324, 5);
+		}
+	}
+
+	if (["food", "booze"].includes(get("_questPartyFairQuest"))) {
+		const partyType = get("_questPartyFairQuest");
+		print(
+			`${partyType} Quest available! Go see Gerald${partyType === "food" ? "ine" : ""}`,
+			"red"
+		);
+	}
+}
+
 export function main(args = ""): void {
-  const newClass = args.includes(`tt`)
-    ? $class`Turtle Tamer`
-    : args.includes(`pm`)
-    ? $class`Pastamancer`
-    : args.includes(`sr`)
-    ? $class`Sauceror`
-    : args.includes(`db`)
-    ? $class`Disco Bandit`
-    : args.includes(`at`)
-    ? $class`Accordion Thief`
-    : $class`Seal Clubber`;
+	const newClass = args.includes(`tt`)
+		? $class`Turtle Tamer`
+		: args.includes(`pm`)
+		? $class`Pastamancer`
+		: args.includes(`sr`)
+		? $class`Sauceror`
+		: args.includes(`db`)
+		? $class`Disco Bandit`
+		: args.includes(`at`)
+		? $class`Accordion Thief`
+		: $class`Seal Clubber`;
 
-  Clan.join("Alliance From Heck");
+	Clan.join("Alliance From Heck");
 
-  prepareAscension({
-    workshed: `Asdon Martin keyfob`,
-    garden: `packet of thanksgarden seeds`,
-    eudora: `New-You Club Membership Form`,
-    chateau: {
-      desk: `continental juice bar`,
-      nightstand: $classes`Pastamancer, Sauceror`.includes(newClass)
-        ? `foreign language tapes`
-        : $classes`Disco Bandit, Accordion Thief`.includes(newClass)
-        ? `bowl of potpourri`
-        : `electric muscle stimulator`,
-      ceiling: `ceiling fan`,
-    },
-  });
+	prepareAscension({
+		workshed: `Asdon Martin keyfob`,
+		garden: `packet of thanksgarden seeds`,
+		eudora: `New-You Club Membership Form`,
+		chateau: {
+			desk: `continental juice bar`,
+			nightstand: $classes`Pastamancer, Sauceror`.includes(newClass)
+				? `foreign language tapes`
+				: $classes`Disco Bandit, Accordion Thief`.includes(newClass)
+				? `bowl of potpourri`
+				: `electric muscle stimulator`,
+			ceiling: `ceiling fan`,
+		},
+	});
 
-  ascend(
-    Paths.Unrestricted,
-    newClass,
-    Lifestyle.casual,
-    "knoll",
-    $item`astral six-pack`,
-    $item`astral shirt`
-  );
+	ascend(
+		$path`Unrestricted`,
+		newClass,
+		Lifestyle.casual,
+		"knoll",
+		$item`astral six-pack`,
+		$item`astral shirt`
+	);
+	NEP();
 }
